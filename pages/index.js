66a5138cd125a92a5cc71534602b640bc8583d3a
Jake <jakem80808@gmail.com>
2023-01-14 23:01:38 -0800

    Minor changes

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,22 @@
 import React, { useState } from 'react';
 import Navbar from '../components/Navbar.jsx';
+import Image from 'next/image'
+import bg from '/public/slowed-spiraling.gif'
+
 
 function Home() {
 
   return (
-    <div className='flex-grow bg-[url("https://media2.giphy.com/media/l0MYGeMQjSqhQ3UaI/giphy.gif?cid=ecf05e47i90gorx3ru7npbs5waqxsr1f7qihv6lf6qekdteg&rid=giphy.gif&ct=g")] bg-cover bg-center'>
+    <div className=".backgroundImage-trippy">
+      {/* <Image
+        src={bg}
+        alt="trippy spiral background"
+        className="w-screen"
+      /> */}
       <h1>Hello, there! Welcome to my project!</h1>
       <Navbar />
     </div>
+
   );
 }
 
