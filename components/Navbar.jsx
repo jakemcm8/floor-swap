9dfd75fff30af6d942cebf9fa4740c1effef2bfd
Jake <jakem80808@gmail.com>
2023-01-16 23:58:34 -0800

    Styling changes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Navbar () {
   return (
-    <div className='font-extrabold opacity-80 text-gray-700 flex flex-col'>
+    <div className='absolute left-1/2 font-extrabold opacity-80 text-gray-700 flex-row mt-96 gap-x-12'>
       <Link href="/lending">Borrow/Lend</Link>
       <Link href="/portfolio">Portfolio</Link>
       <Link href="/staking">Staking</Link>
