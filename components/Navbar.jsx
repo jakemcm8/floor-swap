668df11411aeab0767a0b69c42390965ea1b6a49
Jake <jakem80808@gmail.com>
2023-01-17 22:28:08 -0800

    Navbar changes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,10 +2,13 @@ import Link from 'next/link'
 
 export default function Navbar () {
   return (
-    <div className='absolute left-1/2 font-extrabold opacity-80 text-gray-700 flex-row mt-96 gap-x-12'>
-      <Link href="/lending">Borrow/Lend</Link>
-      <Link href="/portfolio">Portfolio</Link>
-      <Link href="/staking">Staking</Link>
+    <div className="mt-16 grid place-items-center">
+      <div className='grid grid-cols-3 place-items-center font-extrabold text-3xl opacity-80 text-gray-300'>
+        <Link href="/lending">Borrow/Lend</Link>
+        <Link href="/portfolio" className="place-items">Portfolio</Link>
+        <Link href="/staking">Staking</Link>
+      </div>
     </div>
+
   )
 }
