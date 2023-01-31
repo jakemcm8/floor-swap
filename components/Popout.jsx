c289db83a8a0839883722699eb189f5aee7d10e2
Jake <jakem80808@gmail.com>
2023-01-30 18:36:28 -0800

    Changed padding

--- components/Popout.jsx
+++ components/Popout.jsx
@@ -8,13 +8,13 @@ export default function Popout (props) {
   }
 
   return (
-    <div className="fixed block bg-red-500 h-screen w-64 top-0">
+    <div className="fixed block bg-white h-screen w-64 top-0">
       <img src="/close-icon.png" onClick={close} className='absolute right-0'/>
       <div className="flex flex-col font-bold text-2xl">
-        <Link href="/docs" className="pt-4 pb-6">Docs</Link>
-        <Link href="/about" className="py-6">About</Link>
-        <Link href="/careers" className="py-6">Careers</Link>
-        <Link href="https://substack.com/" className="py-6">Blog</Link>
+        <Link href="/docs" className="pt-12 pb-6 px-4">Docs</Link>
+        <Link href="/about" className="py-6 px-4">About</Link>
+        <Link href="/careers" className="py-6 px-4">Careers</Link>
+        <Link href="https://substack.com/" className="py-6 px-4">Blog</Link>
       </div>
     </div>
   )
