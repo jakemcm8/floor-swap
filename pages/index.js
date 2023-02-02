9061b80f1c0d27056ddd23bd796596daba0b52e5
Jake <jakem80808@gmail.com>
2023-02-02 13:18:42 -0800

    Added repeatable background pattern

--- pages/index.js
+++ pages/index.js
@@ -17,20 +17,19 @@ function Home() {
   }
 
   return (
-    <div>
-      <div className='bg-[url("https://images.pexels.com/photos/19670/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")] bg-cover h-screen w-screen font-mono'>
+
+    <div className='bg-[url("../public/backgrounds/spink_splatter.png")] bg-cover h-screen w-screen font-mono'>
 
 
-        {/* If isClosed is false, render Popout and hidden HamButton. If true, render Button*/}
-        {/* If (X) is pressed in the Popout, isClosed changes to true */}
-        {/* isClosed is changed in HamburgerButton when the icon is clicked on */}
+      {/* If isClosed is false, render Popout and hidden HamButton. If true, render Button*/}
+      {/* If (X) is pressed in the Popout, isClosed changes to true */}
+      {/* isClosed is changed in HamburgerButton when the icon is clicked on */}
 
-        <ProfileButton />
-        {isClosed ? <HamburgerButton makeOpen={makeOpen}/> : <><Popout makeClosed={setIsClosed} /></>}
-        <Navbar />
-        <Scrollbar />
+      <ProfileButton />
+      {isClosed ? <HamburgerButton makeOpen={makeOpen}/> : <><Popout makeClosed={setIsClosed} /></>}
+      <Navbar />
+      <Scrollbar />
 
-      </div>
     </div>
 
 
