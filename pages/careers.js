5a39e351df47fcdd2cd237ac9e724a3ec24deb29
Jake <jakem80808@gmail.com>
2023-02-02 19:54:54 -0800

    Fixed broken page

--- pages/careers.js
+++ pages/careers.js
@@ -1,6 +1,11 @@
+import Link from 'next/link'
+
 export default function About () {
   return (
-    <Link href="/" className="text-3xl text-indigo-600 font-mono font-extrabold hover:drop-shadow-lg hover:underline hover:underline-offset-4">Home</Link>
-    <h1>Insert info here</h1>
+    <div>
+      <Link href="/" className="text-3xl text-indigo-600 font-mono font-extrabold hover:drop-shadow-lg hover:underline hover:underline-offset-4">Home</Link>
+      <h1>Insert info here</h1>
+    </div>
+
   )
 }
