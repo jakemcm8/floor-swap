f8419092263e333570313684dbde27685c8576a7
Jake <jakem80808@gmail.com>
2023-02-08 11:42:40 -0800

    added profile image and stats column

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import ImgScrollbar from '../components/ImgScrollbar.jsx'
 
 export default function Portfolio () {
@@ -10,13 +11,19 @@ export default function Portfolio () {
         <div className="text-4xl py-16 text-white flex-none">
           <h1> (0x223...121)'s Portfolio</h1>
         </div>
-        <div className="flex-auto text-white rounded-md bg-gray-600 w-1/3 h-48 relative ">
+        <div className="flex-auto text-white rounded-lg bg-gray-600 w-1/3 h-48 fixed right-0 mr-16 mt-24">
           <h1 className="">Filler</h1>
           <p></p>
         </div>
       </div>
-      <div className="bg-white rounded-full w-16">
-        <p>InsertProfilePhoto</p>
+      <div className="">
+        <Image
+          src="/icons/default-avatar-icon-of-social-media-user-vector.jpg"
+          alt="default pfp"
+          className="rounded-full fixed right-0 top-0 mr-20 mt-[80px]"
+          width={100}
+          height={100}
+        />
       </div>
 
     </div>
