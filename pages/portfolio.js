c2b80d04b7ef08d2f3314346b2f11376e333ef0d
Jake <jakem80808@gmail.com>
2023-02-07 23:48:31 -0800

    Changes to portfolio/profile page

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -8,11 +8,15 @@ export default function Portfolio () {
       {/* <ImgScrollbar /> */}
       <div className="flex flex-col place-items-center">
         <div className="text-4xl py-16 text-white flex-none">
-          <h1>Portfolio</h1>
+          <h1> (0x223...121)'s Portfolio</h1>
         </div>
-        <div className="flex-auto text-white rounded-md bg-white w-1/3 h-48 relative ">
+        <div className="flex-auto text-white rounded-md bg-gray-600 w-1/3 h-48 relative ">
           <h1 className="">Filler</h1>
+          <p></p>
         </div>
+      </div>
+      <div className="bg-white rounded-full w-16">
+        <p>InsertProfilePhoto</p>
       </div>
 
     </div>
