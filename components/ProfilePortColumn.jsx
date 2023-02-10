405b1e602b49917417e531f5ba44c05e1f5464b5
Jake <jakem80808@gmail.com>
2023-02-10 00:05:37 -0800

    Added alternating column bg on profile page

--- components/ProfilePortColumn.jsx
+++ components/ProfilePortColumn.jsx
@@ -2,30 +2,22 @@ import Image from 'next/image'
 
 export default function ProfilePortColumn () {
   return (
-    <div className="grid grid-flow-row text-white rounded-lg bg-gray-600 w-1/3 h-48 fixed right-0 mr-16 mt-28">
-      <div className="odd:bg-red-500 even:bg-green-400">
-        <h1 className="">Filler</h1>
-        <p>test00</p>
-        <p>test00</p>
-        <p>test00</p>
-        <p>test00</p>
-        <p>test00</p>
-        <p>test00</p>
-        <p>test00</p>
-        <p>test00</p>
-        <p>test00</p>
-        <p>test00</p>
-        <p>test00</p>
-        <p>test00</p>
-        <Image
+    <div className="grid grid-flow-row text-white rounded-lg bg-slate-300 w-1/3 h-48 fixed right-0 mr-16 mt-28">
+      <ul>
+        <li className="first:pt-12 odd:bg-slate-300 even:bg-slate-500">Test</li>
+        <li className="odd:bg-slate-300 even:bg-slate-500">Test</li>
+        <li className="odd:bg-slate-300 even:bg-slate-500">Test</li>
+        <li className="odd:bg-slate-300 even:bg-slate-500">Test</li>
+        <li className="odd:bg-slate-300 even:bg-slate-500">Test</li>
+      </ul>
+
+      <Image
         src="/icons/default-avatar-icon-of-social-media-user-vector.jpg"
         alt="default pfp"
         className="rounded-full fixed right-0 top-0 mr-20 mt-[70px]"
         width={150}
         height={150}
-        />
-      </div>
-
+      />
     </div>
   )
 }
